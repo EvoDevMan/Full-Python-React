d2b4a948239399e84725975f8f7cffa009acf819
Trevor Laja <lajat@deea.thebuffalogroup.com>
2020-06-01 17:14:33 +0000

    initial setup fe

---
+++ root_stack/reactive/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './Components/App';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('application-root')
+  );
